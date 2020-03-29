235013f8d0bc2ccb2dc18a29d541235f30648ad4
Rifai Kuçi <rfai4747@hotmail.com>
2020-03-29 22:10:41 +0300

    Initial commit

---
+++ app/src/main/java/com/rifaikuci/alzheimer_tracking/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rifaikuci.alzheimer_tracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
